6e5fa0d7416a56d7e574e56f4f9a9f8d21d5be73
surdarla <rlskejdk123@gmail.com>
2023-11-11 17:56:58 +0900

    done

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # js_clone
+
 Nomad coder js clone challengue
 
 1. 실시간 시계
@@ -8,3 +9,9 @@ Nomad coder js clone challengue
 5. 날씨와 위치 (geolocation)
 6. 여러분의 CSS 실력을 뽐내주세요💖
 7. gitpage deploy
+
+## 순서
+
+- 기본적인 기능을 다 구현한 뒤에 이쁘게 만들 것인지
+- 아니면 구현을 하기 전에 먼저 css까지 생각을 하고 만들 것인지?
+- 굳이 엄청 이쁘게 할 필요는 없으나 tailwindcss는 사용하도록 하자
--- src/index.js
+++ src/index.js
@@ -1,69 +1,136 @@
-const splash = document.querySelector("#splash");
+// 실시간 시계
+function updateClock() {
+  const now = new Date();
+  let year = now.getFullYear();
+  let month = now.getMonth();
+  let day = now.getDay();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
 
-unsplash.photos.getPhoto("")
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
 
-const todoForm = document.querySelector('#todo-form');
-const toDoInput = document.querySelector('#todo-form input');
-const toDoList = document.querySelector('#todo-list');
+  let currentTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  document.querySelector(".clock-text").innerText = currentTime;
+}
+setInterval(updateClock, 1000);
+updateClock();
 
-const toDos = [];
-const TODOS_KEYS = "todos";
+// 랜덤 백그라운드 이미지 from unsplash
+function getRandomImage() {
+  const imagePaths = [
+    "src/img/bg1.jpg",
+    "src/img/bg2.jpg",
+    "src/img/bg3.jpg",
+    "src/img/bg4.jpg",
+    "src/img/bg5.jpg",
+  ];
+  const randomImagePath =
+    imagePaths[Math.floor(Math.random() * imagePaths.length)];
+  document.body.style.background = `url(${randomImagePath})`;
+  document.body.style.backgroundSize = "100% auto";
+}
+getRandomImage();
 
-function paintTodo(newTodo) {
-    const li = document.createElement("li");
-    const span = document.createElement("span");
-    span.innerText = newTodo.text;
-    const button = document.createElement("button");
-    button.innerText = "❌";
-    button.addEventListener("click", deleteTodo);
-    li.appendChild(span);
-    li.appendChild(button);
-    toDoList.appendChild(li);
+// localStorage login
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
 }
 
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
+
+// todo
+
+const todoForm = document.getElementById("todo-form");
+const todoInput = document.querySelector("#todo-form input");
+const todoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "todos";
+
+let todos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+}
 function deleteTodo(event) {
-    const li = event.target.parentElement; //target = html element
-    toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
-    li.remove();
+  const li = event.target.parentElement;
+  li.remove();
+  todos = todos.filter((todos) => todos.id !== parseInt(li.id));
+  saveToDos();
+}
+function paintTodo(newTodo) {
+  const li = document.createElement("li");
+  li.id = newTodo.id;
+  const span = document.createElement("span");
+  span.innerText = newTodo.text;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteTodo);
+  li.appendChild(span);
+  li.appendChild(button);
+  todoList.appendChild(li);
+}
+function handleTodosubmit(event) {
+  event.preventDefault();
+  const newTodo = todoInput.value;
+  todoInput.value = "";
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  todos.push(newTodoObj);
+  paintTodo(newTodoObj);
+  saveToDos();
+}
+todoForm.addEventListener("submit", handleTodosubmit);
+
+const savedTodos = localStorage.getItem(TODOS_KEY);
+if (savedTodos !== null) {
+  const parsedTodos = JSON.parse(savedTodos);
+  todos = parsedTodos;
+  parsedTodos.forEach(paintTodo);
 }
 
-function saveTodos() {
-    localStorage.setItem("todos", JSON.stringify(toDos));
-}
-
-function handleToDoSubmit(event) {
-    event.preventDefault();
-    const newTodo = toDoInput.value;
-    toDoInput.value = '';
-    const newTodoObj = {
-        text: newTodo,
-        id: Date.now(),
-    }
-    toDos.push(newTodoObj);
-    paintTodo(newTodoObj);
-}
-
-const savedToDos = localStorage.getItem(TODOS_KEYS);
-if (savedToDos !== null) {
-    const parsedToDos = JSON.parse(savedToDos);
-    parsedToDos.forEach(paintTodo);
-}
-
-const API_KEY = "";
-function onGeoOk(position) {
-    const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
-    fetch(url).then(response => response.json()).then(data => {
-        const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:last-child");
-        city.innerText = data.name;
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`
-        console.log(data);
-    });
+// weather https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+const API_KEY = "e42915540fe195fc7e55e44636eed48d";
+function onGeoPass(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  fetch(url).then(response => response.json()).then(data => {
+    const weather = document.querySelector("#weather span:first-child");
+    const city = document.querySelector("#weather span:last-child");
+    city.innerText = data.name;
+    weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    console.log(data);
+  });
 }
 function onGeoError() {
-    alert("Cant't find you. No weather for you.");
-
+  alert("Cant get weather for you");
 }
-navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+navigator.geolocation.getCurrentPosition(onGeoPass, onGeoError);
